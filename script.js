692361ea63587b627be21e5a55870c4cb6161666
Dominique Czok <d.czok@web.de>
2022-05-05 14:40:23 +0200

    found last error

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ const counter = document.querySelector(".counter");
 let number = 0;
 
 function result() {
-  counter.innerText = number;
-  main.style.backgroundSize = number + "% 100%";
+    counter.innerText = number;
+    main.style.backgroundSize = number + "% 100%";
 }
 
 function addCount() {
-  if (number < 100) {
-    number++;
-    result();
-  } else {
-    number = 0;
-    result();
-  }
+    if (number < 100) {
+        number++;
+        result();
+    } else {
+        number = 0;
+        result();
+    }
 }
 
 main.addEventListener("click", addCount);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === " " || e.key === "Enter") {
-    addCount();
-  }
+    if (e.key === " " || e.key === "Enter") {
+        addCount();
+    }
 });
 
-button.addEventListener("click", function () {
-  number = 0;
-  result();
-  button.blur();
+button.addEventListener("click", function() {
+    number = 0;
+    result();
+    button.blur();
 });
